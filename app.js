de41a17dc6bb092d7df422ed247efb33005e6011
cabec <cabecao10061@gmail.com>
2022-10-23 15:10:28 -0300

    adicionando console log

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log("Rodando o sistema de gerenciamento de jogos"
-console.log("aplicação esta se conectando ao banco de dados"
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação esta se conectando ao banco de dados")
+// mais um console log de teste:    
+console.log("O paulo fez um commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
+console.log("teste")
